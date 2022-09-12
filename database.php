d5c5a1925a29fff496d320a81fb29d6717a41136
dani <angeldaniel6709@gmail.com>
2022-09-11 20:03:07 -0500

    databases_implementation

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php 
+$host= "localhost";
+$database= "contacts_app";
+$user= "root";
+$password= "";
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
